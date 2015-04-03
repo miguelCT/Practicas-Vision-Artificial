90e0e2ac2b580b0c3a4a5f67377bca8c7c0a35bc
miguelCT <miguel.cortecero.torres@gmail.com>
2015-04-03 12:46:55 +0200

    Nuevas pruebas realizadas y añadida info
    -Ahora se muestran las rectas de cada pto al centro
    -Se ha detectado que como la imgen que se procesa no es del mismo tamaño
    que de la que estamos sacando los kpoints (entrenamiento) que coinciden,
    será necesario redimensionar para adaptar la ubicación de esos ptos a la
    máscara donde marcaremos qué ptos coinciden

--- Practica3/Parte1.py
+++ Practica3/Parte1.py
@@ -4,6 +4,7 @@ import glob
 import os
 import math
 import KeyPoint
+import numpy as np
 
 
 
@@ -18,13 +19,14 @@ def calcularCentro(kp, image):
     centerX = width / 2
     angleKp = kp.angle
     xKp, yKp = kp.pt[:2]
+    pt = (centerX, centerY)
 
-    module = math.sqrt(math.pow((centerX - xKp), 2) + math.pow((centerY - yKp), 2))
+    module = int(math.sqrt(math.pow((centerX - xKp), 2) + math.pow((centerY - yKp), 2)))
     if (centerY - yKp) != 0:
         angle = math.atan((centerX - xKp) / (centerY - yKp))
     else:
         angle = 0
-    distanteToCenterPolar = (module, angle)
+    distanteToCenterPolar = (module, angle, pt)
     return distanteToCenterPolar
 
 #Entrenamiento con imagenes de prueba
@@ -37,15 +39,21 @@ def training ():
         ownKP=[]
         for kp in kpA:
             distanteToCenterPolar = calcularCentro(kp, I)
-            keyPoint = KeyPoint.KeyPoint(kp.angle, distanteToCenterPolar, kp.size, kp.pt)
+            # Para obtener la posicion en integer
+            x, y = kp.pt[:2]
+            pos = (int(x), int(y))
+            keyPoint = KeyPoint.KeyPoint(kp.angle, distanteToCenterPolar, kp.size, pos)
             ownKP.append(keyPoint)
+            #Obtenemos el centro de la imagen
+            dCMdl, dCAgl, dCPt =  keyPoint.distanceToCenter[:3]
+            cv2.line(I, keyPoint.position, dCPt, (255, 255, 0) , thickness=2, lineType=8, shift=0)
 
+        # Guardamos los kpoint y la resolucion de la imagen
         arrayOwnKeyPoints.append((I.shape, ownKP))
         #Guardamos los kp y los descriptores de cada imagen de entrenamiento para su posterior uso
         descArray.append(desA)
         kpArray.append(kpA)
-
-        img2 = cv2.drawKeypoints(I, kpA, color=(0, 255, 0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        I = cv2.drawKeypoints(I, kpA, color=(0, 255, 0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         # cv2.imshow("Result", I)
         # cv2.waitKey()
         # cv2.imshow("Result2", img2)
@@ -70,25 +78,51 @@ def processing():
         index = 0
         for file in glob.glob("*.jpg"):
 
-            I = cv2.imread(file, 0)  # Caragar imagen en
+            I = cv2.imread(file, 0)  # Caragar imagen en blanco y negro
 
             orb = cv2.ORB(nfeatures=100, nlevels=4, scaleFactor=1.3)
             kpA, desA = orb.detectAndCompute(I,None)
+            I = cv2.drawKeypoints(I, kpA, color=(0, 255, 0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            # cv2.imshow("Processing kp", img2)
+            # cv2.waitKey()
+
             matches = flann.knnMatch(desA,descArray[index],k=2)
             print "------------- imagen -------------"
             matchesMask = [[0,0] for i in xrange(len(matches))]
+            xImage,yImage = I.shape[:2]
+            xImage = xImage/10
+            yImage = yImage/10
+            # Creamos la mascara donde iremos anotando los ptos que coinciden
+            mask = np.zeros((xImage,yImage),np.uint8)
+
             for match in matches:
                 for desc in match:
                     kp = kpA[desc.queryIdx]
                     distanteToCenterPolar = calcularCentro(kp, I)
-                    keyPoint = KeyPoint.KeyPoint(kp.angle, distanteToCenterPolar, kp.size, kp.pt)
+                    # Posicion en integer
+                    x, y = kp.pt[:2]
+                    pos = (int(x), int(y))
+                    keyPointProcessing = KeyPoint.KeyPoint(kp.angle, distanteToCenterPolar, kp.size, pos)
+                    #Obtenemos el centro de la imagen
+                    dCMdl, dCAgl, dCPt =  keyPointProcessing.distanceToCenter[:3]
+                    cv2.line(I, keyPointProcessing.position, dCPt, (255, 255, 0) , thickness=2, lineType=8, shift=0)
                     trainResolution, kpoints = arrayOwnKeyPoints[index][:2]
-                    # print "Nueva res", I.shape
-                    # print "Antigua res", trainResolution
-                    print "Antiguo nuestro", kpoints[desc.queryIdx]
-                    print "Nuevo nuestro", keyPoint
+                    x, y = keyPointProcessing.position[:2]
+                    x = x/10
+                    y = y/10
+                    # Hay un problema ya que la imagen con la que creamos la mascara y la imagen de la que se han sacado los kpoint no es del mismo tamanno y por tanto no
+                    # podremos usar la mascara como tal, habra que redimensionar
+                    mask[x][y] += 1
+                    # print "Antiguo nuestro", kpoints[desc.queryIdx]
+                    # print "Nuevo nuestro", keyPoint
 
+            print "Nueva resolucion", I.shape
+            print "Antigua resolucion", trainResolution
 
+            print mask
+            cv2.imshow("Processing kp", I)
+            cv2.imshow("Processing mask", mask)
+            cv2.waitKey()
         index += 1
 
 training()
