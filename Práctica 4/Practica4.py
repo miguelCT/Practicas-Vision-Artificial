f6a024f10421f168799ef4abdfda2ac3b1173376
edutodi <edutodi@hotmail.com>
2015-04-25 20:04:52 +0200

    Practica 4. Matriz de caracteristicas y vector de etiquetas

--- Práctica 4/Practica4.py
+++ Práctica 4/Practica4.py
@@ -5,6 +5,9 @@ import glob
 import os
 import cv2.cv as cv
 
+C_TAM_MAXIMO_CONTORNO = 10
+
+
 #Busca los rectangulos donde cree que esta la imagen
 def detect(img, cascade, vecinos, size):
     rects = cascade.detectMultiScale(img, scaleFactor=1.2, minNeighbors=vecinos, minSize=(size, size), flags=cv.CV_HAAR_SCALE_IMAGE)
@@ -65,7 +68,7 @@ def filtrarContornos(contornos,matricula):
 
 
         #Verficar que es alto que ancho y que tiene mas de 10 x 10 pixeles
-        if (((ladoXContorno) < (ladoYContorno)) & ((ladoXContorno) > 10) & ((ladoYContorno) > 10)) and \
+        if (ladoXContorno < ladoYContorno) & (ladoYContorno > C_TAM_MAXIMO_CONTORNO) and \
             (x1Contorno >= x1Matricula) & (x2Contorno <= x2Matricula) & \
             (y1Contorno >= y1Matricula) & (y2Contorno <= y2Matricula):
 
@@ -78,6 +81,22 @@ def umbralizarImagen(imagen):
     imagenUmbralizada=cv2.adaptiveThreshold(imagen,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv2.THRESH_BINARY_INV,75,10)
     return imagenUmbralizada
+
+def imprimirMatrizDeGrises(imagen):
+    for fila in imagen:
+        for columna in fila:
+            print '{:4}'.format(columna),
+        print
+
+
+def transformarMatrizDeGrises(imagen):
+    vectorDeCaracteristicas = []
+
+    for fila in imagen:
+        for elemento in fila:
+            vectorDeCaracteristicas.append(elemento)
+
+    return vectorDeCaracteristicas
 
 def testing():
     os.chdir("./testing_ocr")
@@ -106,23 +125,70 @@ def testing():
             cv2.rectangle(imagenUmbralizada, (x1Matricula, y1Matricula), (x2Matricula, y2Matricula), (255, 255, 255))
 
             #FIN BORRAR
+
             contornosEnMatricula=filtrarContornos(contornos,matricula)
 
-            if len(contornosEnMatricula) != 0:
-                pintarContornos(imagenUmbralizada,contornosEnMatricula)
-            else:
+            imagenContornos = np.copy(imagenUmbralizada)
+            if len(contornosEnMatricula) == 0:
                 print("Contornos vacios")
+            else:
+                pintarContornos(imagenContornos,contornosEnMatricula)
 
-        cv2.imshow("Contornos", imagenUmbralizada)
-        cv2.waitKey()
+                cv2.imshow("Contornos",imagenContornos)
+
+
+                # for contorno in contornosEnMatricula:
+                #
+                #     x,y,w,h = cv2.boundingRect(contorno)
+                #     x1Contorno = x
+                #     y1Contorno = y
+                #     x2Contorno = x+w
+                #     y2Contorno = y+h
+                #
+                #
+                #     imagenContorno = np.copy(imagen)
+                #     imagenContorno = imagenContorno[y1Contorno:y2Contorno, x1Contorno:x2Contorno]
+                #
+                #     imagenContorno = cv2.resize(imagenContorno,(300,300))
+                #
+                #     contornoEnImagen = np.copy(imagenUmbralizada)
+                #
+                #     imagenContorno = umbralizarImagen(imagenContorno)
+                #
+                #     cv2.rectangle(contornoEnImagen, (x1Contorno, y1Contorno), (x2Contorno, y2Contorno), (255, 0, 255))
+                #     cv2.imshow("contornoEnImagen", contornoEnImagen)
+                #
+                #     cv2.imshow(("Contorno Recortado"), imagenContorno)
+                #
+                #
+                #     imagenContorno = cv2.resize(imagenContorno,(10,10))
+                #     cv2.imshow(("Contorno Recortado 10x10"), imagenContorno)
+                #
+                #
+                #     #imprimirMatrizDeGrises(imagenContorno)
+                #
+                #     vectorDeCaracteristicas = transformarMatrizDeGrises(imagenContorno)
+                #
+                #     print(vectorDeCaracteristicas)
+                #     print len(vectorDeCaracteristicas)
+                cv2.waitKey()
 
 def training():
-    os.chdir("../training_ocr")
+    os.chdir("./training_ocr")
+
+    indexMatrizCaracteristicas = 0
+    c = []
+    e = []
+
     for file in glob.glob("*.jpg"):
+
+
 
         imagen = cv2.imread(file,0)
 
         imagenUmbralizada = umbralizarImagen(imagen)
+
+        cv2.imshow("Imagen Umbralizada", imagenUmbralizada)
 
         contornos, jerarquiaContornos=cv2.findContours(imagenUmbralizada, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
@@ -138,18 +204,40 @@ def training():
 
             contornosEnNumero= []
             #Verficar que es alto que ancho y que tiene mas de 10 x 10 pixeles
-            if (((ladoXContorno) < (ladoYContorno)) & ((ladoXContorno) > 10) & ((ladoYContorno) > 10)):
+            if ((ladoXContorno) < (ladoYContorno)) & ((ladoXContorno) > 10) & ((ladoYContorno) > 10):
                 contornosEnNumero.append(contorno)
+
+
 
         pintarContornos(imagenUmbralizada,contornosEnNumero)
 
-        cv2.imshow("Imagen Umbralizada", imagenUmbralizada)
-        cv2.waitKey()
+
+        cv2.imshow("Imagen Umbralizada con contornos", imagenUmbralizada)
+
+        imagenContorno = imagenUmbralizada[y1Contorno:y2Contorno, x1Contorno:x2Contorno]
+        imagenContorno = cv2.resize(imagenContorno,(10,10))
+
+        cv2.imshow("Imagen rescalada", imagenContorno)
+
+        vectorDeCaracteristicas = transformarMatrizDeGrises(imagenContorno)
+
+        #Insertar clase en E y el vector de caracteristicas en C
+        e.insert(len(e),file[0])
+        c.insert(len(c),vectorDeCaracteristicas)
+
+
+
+    for fila in c:
+        print(fila)
+    print(len(c))
+    print(e)
+
+        #cv2.waitKey()
 
 
 
 def main():
-    testing()
+    #testing()
     training()
 
 main()
